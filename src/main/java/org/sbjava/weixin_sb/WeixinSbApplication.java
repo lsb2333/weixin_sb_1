fae3dd72ee44e1661f85b14f9bc7205f9c7ccb40
lsb <lsb@lsb-PC>
2019-03-17 15:35:28 +0800

    初次提交Spring Boot的项目模板

---
+++ src/main/java/org/sbjava/weixin_sb/WeixinSbApplication.java
@@ -0,0 +1,13 @@
+package org.sbjava.weixin_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixinSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeixinSbApplication.class, args);
+	}
+
+}
