faf3bbbb54ba9e84bdb3d4686aed99e65213e42b
lsb <lsb@lsb-PC>
2019-03-27 13:09:38 +0800

    接收微信公众号豪平台发送给我们的消息

--- src/main/java/org/sbjava/weixin_sb/controller/MessageReceiverController.java
+++ src/main/java/org/sbjava/weixin_sb/controller/MessageReceiverController.java
@@ -1,9 +1,14 @@
 package org.sbjava.weixin_sb.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 // 控制器 : 负责接收用户的请求参数、调用业务逻辑层代码、返回视图/结果给客户端（浏览器）
 // @Controller  基于JSP的控制器
@@ -14,6 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/lsb/weixin/receiver")
 public class MessageReceiverController {
 
+	//日志记录器
+	private static final Logger LOG = LoggerFactory.getLogger(MessageReceiverController.class);
+	
+	
 	@GetMapping // 只处理GET请求
 	public String echo(//
 			@RequestParam("signature") String signature, //
@@ -28,6 +37,24 @@ public class MessageReceiverController {
 
 		// 原路返回echostr的值，返回以后微信公众号平台就能够认为：服务器对接成功
 		return echostr;
+	}
+	
+	
+	@PostMapping
+	//@RequestBody注解表示把请求内容获取出来，并且 转换为String传入给xml参数
+	public String onMessage(
+			@RequestParam("signature") String signature, //
+			@RequestParam("timestamp") String timestamp, //
+			@RequestParam("nonce") String nonce, //
+			@RequestBody String xml) {
+		//接收消息
+		LOG.trace("接收的消息原文：\n{}\n----------------------------",xml);
+		//转换消息
+		//把消息丢入队列
+		//消费队列中的消息
+		//产生客服消息
+		
+		return "success";
 	}
 }
 
