b01d8766bcecd50bfb21d13f1737f1480577282f
lsb <lsb@lsb-PC>
2019-03-31 18:08:00 +0800

    解析消息类型

--- src/main/java/org/sbjava/weixin_sb/controller/MessageReceiverController.java
+++ src/main/java/org/sbjava/weixin_sb/controller/MessageReceiverController.java
@@ -60,7 +60,10 @@ public class MessageReceiverController {
 		
 		
 		//截取消息类型
-		String type = xml.substring(0);
+		//<MsgType><![CDATA[text]]></MsgType>
+		String type = xml.substring(xml.indexOf("<MsgType><![CDATA[") + 18);
+		type = type.substring(0, type.indexOf("]]></MsgType>"));
+		
 		Class<InMessage> cla = MessageTypeMapper.getClass(type);
 		
 		//使用JAXB完成XML转换为java对象的操作 
